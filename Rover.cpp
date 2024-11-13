530686250fa179d53f5551c1d83ca70ea5bfb74b
michaelwithnospaces <michaelwithnospaces@proton.me>
2024-11-13 15:14:24 -0600

    fix: formatting

--- Rover.cpp
+++ Rover.cpp
@@ -4,31 +4,38 @@
 
 using std::cout, std::endl;
 
-void Rover::print() {
+void Rover::print() 
+{
 	cout << savedSMILE << endl; // MyString: operator<<()
 }
 
-void Rover::read(int n) {
+void Rover::read(int n) 
+{
 	cout << savedSMILE.at(n) << endl; // MyString: at()
 }
 
-void Rover::clear() {
-	if (!savedSMILE.empty()) { // MyString: empty()
+void Rover::clear() 
+{
+	if (!savedSMILE.empty()) // MyString: empty()
+	{ 
 		savedSMILE.clear(); // MyString: clear()
 	}
 }
 
-void Rover::join(MyString input) {
+void Rover::join(MyString input) 
+{
 	savedSMILE += input; // MyString: operator+=()
 }
 
-MyString Rover::test(MyString input) {
+MyString Rover::test(MyString input) 
+{
 	MyString tester(savedSMILE); // MyString: copy constructor
 	tester += input; // MyString: operator+=()
 	return tester; // MyString: destructor
 }
 
-bool Rover::find(MyString search) {
+bool Rover::find(MyString search) 
+{
 	int index = savedSMILE.find(search);
 	return index >= 0; // MyString: find()
 }
