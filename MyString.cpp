73e0e3a027c9703249a1c28116219c4f91a0c20c
michaelwithnospaces <michaelwithnospaces@proton.me>
2024-11-13 15:39:05 -0600

    feat: default constructor, copy, from cstr constructor

--- MyString.cpp
+++ MyString.cpp
@@ -1,10 +1,33 @@
 # include "MyString.h"
 
-MyString::MyString() : string("") {};
+MyString::MyString() : cstring_("") {};
 
-MyString::MyString (const MyString& strToCopy) : string(strToCopy.string) {};
+MyString::MyString (const MyString& strToCopy) : cstring_(strToCopy.cstring_) {};
 
 MyString::MyString (const char* cstr)
 {
-    
+    // if cstr is non empty
+    if (cstr != nullptr)
+    {
+        this->length_ = 0;
+        // find length by iterating until not null terminator
+        while (cstr[this->length_] != '\0')
+        {
+            ++(this->length_);
+        }
+
+        cstring_ = new char[this->length_ + 1]; // +1 for null terminator
+
+        for (int i = 0; i < length_; ++i)
+        {
+            this->cstring_[i] = cstr[i];
+        }
+        this->cstring_[length_] = '\0'; // null terminate the string
+    }
+    else
+    {
+        this->length_ = 0;
+        this->cstring_ = new char[1];
+        this->cstring_[0] = '\0';
+    }
 }
