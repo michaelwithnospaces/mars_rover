7f6d20b37d84325130b00e4d37d03b22a2acbfce
michaelwithnospaces <michaelwithnospaces@proton.me>
2024-11-14 12:59:41 -0600

    fix: run compilation and files

--- run.sh
+++ run.sh
@@ -6,17 +6,7 @@ g++ -std=c++17 -g -Wall -Wextra -pedantic-errors -Weffc++ -Wno-unused-parameter 
 # Check if compilation was successful
 if [ $? -eq 0 ]; then
     echo "Compilation successful."
-    
-    # Prompt the user for the input file
-    read -p "Enter the input file name (e.g., commands1.txt): " input_file
-    
-    # Check if the file exists
-    if [ -f "$input_file" ]; then
-        echo "Running the program with input from $input_file..."
-        ./a.out < "$input_file" # Run the program with the specified input file
-    else
-        echo "Error: File '$input_file' not found."
-    fi
+    ./a.out
 else
     echo "Compilation failed."
 fi
